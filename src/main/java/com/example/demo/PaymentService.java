bd2e4c2001fd192d095dfe1094c90a8f74cee16f
hyein <kimnieyh@gmail.com>
2025-04-01 18:48:11 +0900

    클래스의 분리

--- src/main/java/com/example/demo/PaymentService.java
+++ src/main/java/com/example/demo/PaymentService.java
@@ -1,26 +1,22 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-abstract public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+public class PaymentService {
+    private final WebApiExRateProvider exRateProvider;
+
+    public PaymentService() {
+        this.exRateProvider = new WebApiExRateProvider();
+    }
+
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {        
+        BigDecimal exRate = exRateProvider.getWebExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
-        return new Payment(orderId,currency,foreignCurrencyAmount, exRate, convertedAmount,
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount,
                 validUntil);
     }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
-
 }
--- src/main/java/com/example/demo/SimpleExRateProvider.java
+++ src/main/java/com/example/demo/SimpleExRateProvider.java
@@ -3,9 +3,8 @@ package com.example.demo;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRatePaymentService extends PaymentService {
+public class SimpleExRateProvider {
 
-    @Override
     BigDecimal getExRate(String currency) throws IOException {
         if(currency.equals("USD")) return BigDecimal.valueOf(1000);
 
